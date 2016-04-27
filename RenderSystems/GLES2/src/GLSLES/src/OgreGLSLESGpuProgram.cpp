fec5f8b7cd927c7fc5f5f6f004767a52dd7532b1
Luo Jinghua <sunmoon1997@gmail.com>
2016-04-27 08:57:41 +0800

    Fix a crash while removing gpu program from program manager

--- RenderSystems/GLES2/src/GLSLES/src/OgreGLSLESGpuProgram.cpp
+++ RenderSystems/GLES2/src/GLSLES/src/OgreGLSLESGpuProgram.cpp
@@ -64,8 +64,10 @@ namespace Ogre {
     GLSLESGpuProgram::~GLSLESGpuProgram()
     {
 		// This workaround is needed otherwise we carry on some dangling pointers
-		GLSLESLinkProgramManager::getSingletonPtr()->destroyAllByProgram(this);
-		GLSLESProgramPipelineManager::getSingletonPtr()->destroyAllByProgram(this);
+		if (GLSLESLinkProgramManager::getSingletonPtr())
+			GLSLESLinkProgramManager::getSingletonPtr()->destroyAllByProgram(this);
+		if (GLSLESProgramPipelineManager::getSingletonPtr())
+			GLSLESProgramPipelineManager::getSingletonPtr()->destroyAllByProgram(this);
 
         // Have to call this here rather than in Resource destructor
         // since calling virtual methods in base destructors causes crash
